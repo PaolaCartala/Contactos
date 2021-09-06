d668cb98e8c49d804d2cc388f25db34931e7273d
Paola Cartala <paola.cartala@gmail.com>
2021-09-06 14:25:20 -0300

    docstring agregados

---
+++ README.md
@@ -0,0 +1,60 @@
+# CRUD Contactos del teléfono
+
+[![Gitter](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=darkgreen)](https://www.python.org/)
+[![Gitter](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/paola-cartala/)
+
+## Descripción
+
+Esta aplicación simula el manejo de contactos en un teléfono usando el lenguaje Python, creando contactos nuevos, mostrándolos, modificándolos y eliminándolos cuando se necesite.
+
+## Instalación
+
+No es necesario instalar ningún paquete externo, el módulo os se importa automáticamente en el archivo.
+
+## Uso
+
+El programa muestra un menú donde el usuario elige con un número que opción quiere realizar.
+
+Para agregar un nuevo contacto se elige el número 1 y se determina el nombre, número y categoría del contacto, que se guardará como texto plano en la carpeta Contactos, la cual se creará si no existe:
+
+![1- Agregar](img/agregar.jpg)
+
+Para editar el contacto, el programa pedirá un nombre de contacto y lo buscará en la carpeta, si el contacto existe pedirá el nuevo nombre, teléfono y categoría:
+
+![2- Editar](img/editar.jpg)
+
+La opción de ver contactos mostrará todos los contactos que el usuario tiene guardados hasta el momento:
+
+![3- Ver](img/ver.jpg)
+
+La opción buscar pedirá el nombre del contacto que se desea mostrar e imprimirá su información:
+
+![4- Buscar](img/buscar.jpg)
+
+A su vez, la opción eliminar pedirá el nombre del contacto que se quiere eliminar y lo hará, en este caso el contacto no existía:
+
+![5- Eliminar](img/eliminar.jpg)
+
+Por último, la opción 6 permite salir del programa.
+
+## Soporte
+
+Si necesitas soporte sobre el programa, podes [comunicarte](paola.cartala@gmail.com "Enviame un mail!") conmigo.
+
+## Roadmap
+
+Las ideas que tengo para futuras versiones son:
+
+- Agregar confirmación de eliminación de contacto
+
+## Contribuciones
+
+Este repositorio está abierto a contribuciones, no dudes en hacerlo!
+
+## Autores y agradecimientos
+
+Este código fue originalmente escrito por Juan Pablo De la torre Valdez de https://codigoconjuan.com/ a través de [Udemy](https://www.udemy.com/).
+
+## Licencia
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import os # esta es la librería que se usará para administrar los archivos
+import os
 
 # -- Valores que se usarán constantemente --
-CARPETA = "Contactos/" # la carpeta Contactos/ será referida como CARPETA
-EXTENSION = ".txt" # la extension de los archivos utilizados siempre será .txt
+CARPETA = "Contactos/"
+EXTENSION = ".txt"
 
 # --- Clase Contacto ---
-class Contacto: # estos serán los datos que se guardarán en los archivos
+class Contacto:
+    """
+    Clase que contendrá los atributos que se guardarán en los contactos
+    """
     def __init__(self, nombre, telefono, categoria):
         self.nombre = nombre
         self.telefono = telefono
@@ -13,8 +16,11 @@ class Contacto: # estos serán los datos que se guardarán en los archivos
 
 # --- Función principal que contendrá todas las opciones ---
 def app():
+    """
+    Contiene las funciones que muestran el menú principal y crean la carpeta y los archivos con los contactos
+    """
     crear_directorio()
-    mostrar_menu() # muestra el menú de opciones
+    mostrar_menu()
 
     # --- Elección de opciones por parte del usuario ---
     preguntar = True
@@ -23,7 +29,7 @@ def app():
         # -- Ejecutar opciones --
         if opcion == 1:
             agregar_contacto()
-            preguntar = False # con este cambio no preguntará más una vez elegida una opción
+            preguntar = False
         elif opcion == 2:
             editar_contacto()
             preguntar = False
@@ -37,13 +43,16 @@ def app():
             eliminar_contacto()
             preguntar = False
         elif opcion == 6:
-            preguntar = False # salir del bucle sin hacer nada
+            preguntar = False
         else:
-            print("Opción no válida, intente de nuevo") # en caso de que se ingrese un número que no esté entre las opciones, volverá a preguntar qué opción quiere elegir
+            print("Opción no válida, intente de nuevo")
 
 # --- Crea el directorio donde se guardarán los contactos ---
 def crear_directorio():
-    if not os.path.exists(CARPETA): # si la carpeta no existe, la creamos
+    """
+    Verifica si la carpeta Contactos fue creada anteriormente, sino la crea
+    """
+    if not os.path.exists(CARPETA):
         os.makedirs(CARPETA)
 
 # --- Muestra el menú de opciones ---
@@ -58,93 +67,100 @@ def mostrar_menu():
 
 # --- C CREATE / Agregar contactos ---
 def agregar_contacto():
+    """
+    Pide los datos que queremos guardar en el contacto, valida si el archivo existe antes de crearlo, si no existe lo crea y escribe los datos instanciados de la clase Contacto, luego muestra un mensaje de éxito. Si el contacto ya existía también muestra un mensaje de aviso.
+    """
     print("Escribe los datos para agregar el nuevo contacto:")
-    nombre_contacto = input("Nombre del Contacto:\n").capitalize() # utiliza el método capitalize() para poner en mayúscula la primera letra de cada palabra ingresada, por si el usuario las ingresa con minúscula
-    existe = os.path.isfile(CARPETA + nombre_contacto + EXTENSION) # valida si el archivo ya existe antes de crearlo para que no se reescriba
+    nombre_contacto = input("Nombre del Contacto:\n").capitalize()
+    existe = os.path.isfile(CARPETA + nombre_contacto + EXTENSION)
     if not existe:
-        with open(CARPETA + nombre_contacto + EXTENSION, "w", encoding="utf8") as archivo: # abre el archivo con permiso de escritura
+        with open(CARPETA + nombre_contacto + EXTENSION, "w", encoding="utf8") as archivo:
             telefono_contacto = int(input("Agrega el teléfono del contacto:\n"))
             categoria_contacto = input("Agrega la categoría del contacto:\n").capitalize()
 
-            # instancia la clase Contacto
-            contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto) # los argumentos serán los datos que ingresó el usuario
+            contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto)
             
-            # write escribirá los datos del objeto en archivo
             archivo.write ("Nombre: " + contacto.nombre + "\n")
             archivo.write ("Telefono: " + str(contacto.telefono) + "\n")
             archivo.write ("Categoria: " + contacto.categoria + "\n")
 
-            # muestra un mensaje de exito
             print("\nContacto creado correctamente\n")
-    else: print("Ese contacto ya existe") # valida para que no reescriba
-    # reinicia la app despues de agregar o no
+    else: print("Ese contacto ya existe")
     app()
 
 # --- U UPDATE / Editar contactos ---
 def editar_contacto():
+    """
+    Verifica si el contacto existe y lo edita instanciando nuevamente la Clase Contacto, luego cambia el nombre del archivo. Si no existe, muestra un mensaje de aviso
+    """
     nombre_anterior = input("Nombre del contacto que desea editar: \n")
-    existe = existe_contacto(nombre_anterior) # la función existe_contacto() retornará True o False
-    if existe: # si existe es True, ingresará al if
+    existe = existe_contacto(nombre_anterior)
+    if existe:
         with open(CARPETA + nombre_anterior + EXTENSION, "w", encoding="utf8") as archivo:
-            # solicita los datos nuevos y los guarda en las variables
             nombre_contacto = input("Agrega el nuevo nombre: \n")
             telefono_contacto = input("Agrega el nuevo teléfono:\n")
             categoria_contacto = input("Agrega la nueva categoría:\n")
 
-            # instancia la clase Contacto
             contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto)
 
-            # escribe los cambios guardados en las variables en el archivo
-            archivo.write (f"Nombre: {contacto.nombre}\n") # concatena y formatea el string con otro método diferente de agregar_contacto()
+            archivo.write (f"Nombre: {contacto.nombre}\n")
             archivo.write (f"Telefono: {contacto.telefono}\n")
             archivo.write (f"Categoria: {contacto.categoria}\n")
 
-            # muestra mensaje de exito
             print("\nContacto modificado correctamente \n")
 
-        # renombra el archivo si se cambió el nombre del contacto
-        os.rename(CARPETA + nombre_anterior + EXTENSION, CARPETA + nombre_contacto + EXTENSION) # metodo rename, primer argumento es el archivo existente y el segundo es el nombre nuevo del contacto
-    else: # si la variable existe es False, imprime un aviso
+        os.rename(CARPETA + nombre_anterior + EXTENSION, CARPETA + nombre_contacto + EXTENSION)
+    else:
         print("El contacto no existe")
     
     app()
 
-def existe_contacto(nombre): # funcion que retornará un valor, utilizada en editar_contacto() para verificar si existe el contacto antes de editarlo
-    return os.path.isfile(CARPETA + nombre + EXTENSION) # retorna True o False
+def existe_contacto(nombre):
+    """
+    Verifica si el archivo con el nombre de contacto existe en la carpeta
+    """
+    return os.path.isfile(CARPETA + nombre + EXTENSION)
 
 # --- R READ / Mostrar contactos ---
 def mostrar_contactos():
-    # selecciona todos los archivos de la carpeta, como se haría con una base de datos, y los guarda en la variable archivos
-    archivos = os.listdir(CARPETA) # almacena en archivos una lista con los nombres de los archivos del path CARPETA
-    archivos_txt = [i for i in archivos if i.endswith(EXTENSION)] # valida que solamente los archivos que terminen con .txt sean utilizados, aquí se pondría la consulta SQL en caso de utilizar una base de datos
+    """
+    Selecciona todos los archivos de la carpeta, como se haría con una base de datos, y los guarda en la variable archivos, luego recorre cada archivo validado como .txt y lo muestra
+    """
+    archivos = os.listdir(CARPETA)
+    archivos_txt = [i for i in archivos if i.endswith(EXTENSION)]
     print("--- Contactos ---\n")
-    for archivo in archivos_txt: # recorre cada archivo validado como txt
+    for archivo in archivos_txt:
         with open(CARPETA + archivo, encoding="utf8") as contacto:
             for i in contacto:
-                print(i.rstrip()) # rstrip elimina los espacios en blanco
-            print("\n") # separa cada contacto con un salto de linea
-
-# --- Buscar contacto ---
-def buscar_contacto():
-    nombre = input("Seleccione el contacto que desea buscar: \n")
-    # excepciones, para que el programa no caiga cuando se busca un contacto que no existe
-    try: # intenta abrir el archivo con el nombre que ingresa el usuario
-        with open(CARPETA + nombre + EXTENSION, encoding="utf8") as contacto:
-            print("\nInformación del contacto: \n")
-            for i in contacto: # con el mismo código que usa al editar, recorre la carpeta
                 print(i.rstrip())
             print("\n")
 
-    except IOError: # si el contacto no existe, mostrará un mensaje
+# --- Buscar contacto ---
+def buscar_contacto():
+    """
+    Utiliza el manejo de excepciones para buscar un archivo con determinado nombre en la carpeta Contactos, si el archivo existe muestra su contenido, si no muestra un mensaje.
+    """
+    nombre = input("Seleccione el contacto que desea buscar: \n")
+    try:
+        with open(CARPETA + nombre + EXTENSION, encoding="utf8") as contacto:
+            print("\nInformación del contacto: \n")
+            for i in contacto:
+                print(i.rstrip())
+            print("\n")
+
+    except IOError:
         print("El contacto no existe\n")
 
     app()
 
 # --- D DELETE / Eliminar contacto ---
 def eliminar_contacto():
+    """
+    Intenta buscar el contacto que ingresa el usuario y si existe lo elimina, si no muestra un mensaje.
+    """
     nombre = input("Seleccione el contacto que desea eliminar: \n")
     try:
-        os.remove(CARPETA + nombre + EXTENSION) # remove elimina un archivo con el nombre pasado como argumento
+        os.remove(CARPETA + nombre + EXTENSION)
         print("\nEliminado correctamente\n")
     except:
         print("\nNo existe el contacto\n")
